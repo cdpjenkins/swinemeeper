66b56b536fb8e27867d73e3d4cebc98b5d65a368
Chris Jenkins <cdpj@husston.(none)>
2010-08-23 06:52:55 +0100

    update version num to 0.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swinemeeper "0.8"
+(defproject swinemeeper "0.9"
     :dependencies [[org.clojure/clojure "1.2.0"]
                    [org.clojure/clojure-contrib "1.2.0"]
                    [compojure "0.4.0"]
