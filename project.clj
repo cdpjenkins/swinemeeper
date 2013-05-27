63fd48f314f8ff15819d6e610a25bc5ca9e5373b
Chris Jenkins <cdpjenkins@gmail.com>
2013-05-27 23:19:16 +0100

    Add the very beginnings of some rewritten web esque stuff

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
     :dependencies [[org.clojure/clojure "1.5.1"]
                    [org.clojure/clojure-contrib "1.2.0"]
                    [compojure "0.4.0"]
-                   [hiccup "0.2.6"]
+                   [hiccup "1.0.2"]
                    [ring/ring-core "0.2.3"]
                    [ring/ring-jetty-adapter "0.2.3"]]
 
     :dev-dependencies [[swank-clojure "1.2.1"]]
 
-    :main gui)
+    :main swinemeeper.core)
--- src/swinemeeper/core.clj
+++ src/swinemeeper/core.clj
@@ -1,4 +1,28 @@
-(ns swinemeeper.core)
+(ns swinemeeper.core
+  (:use swinemeeper.board
+        [compojure.core :only (defroutes GET)]
+        [ring.adapter.jetty :as ring]
+        [hiccup.core]))
 
+(defn index []
+  (html
+   [:html
+    [:head
+     [:title "I am a title"]]
+    [:body
+     [:h1 "Hello!"]
+     "This is about it, to be honest"
+     [:div {:id "board"}
+      "This is where the board would go. If there was one"]]]))
 
+(defroutes routes
+  (GET "/" [] (index)))
 
+(defn make-server []
+  (let [s (atom (run-jetty (var routes) {:port 8080 :join? false}))]
+;   (.stop @s)
+   s))
+
+(defn -main []
+ ; (run-jetty routes {:port 8080 :join? false})
+  (make-server))
