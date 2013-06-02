997a4fd28675ee1869966992b9dc787f858a0420
Chris Jenkins <cdpjenkins@gmail.com>
2013-06-02 23:32:13 +0100

    require rather than use domina lib

--- cljs_src/swinemeeper/cljs/script.cljs
+++ cljs_src/swinemeeper/cljs/script.cljs
@@ -1,20 +1,20 @@
 (ns swinemeeper.cljs.script
-  (:use [domina :only [by-id value set-text! text xpath set-attr!]])
-  (:require [domina.events :as ev])
+  (:require [domina :as dom]
+            [domina.events :as ev])
 
   )
 
 (.write js/document "Hello, ClojureScript!")
 
 (defn do-stuff []
-  (let [board (by-id "board")]
-        (set-text! board "ston"))
+  (let [board (dom/by-id "board")]
+        (dom/set-text! board "ston"))
   (doseq [i (range 9)]
-    (let [img ( by-id (str i))]
-      (.log js/console img)
-      (set-attr! img "src" (str "images/" (mod (inc i) 9) ".png")))))
+    (let [img (dom/by-id (str i))]
+      (dom/log img)
+      (dom/set-attr! img "src" (str "images/" (mod (inc i) 9) ".png")))))
 
-(.log js/console (str "Waaaaston"))
+(dom/log (str "Waaaaston"))
 (set! (.-onload js/window) do-stuff)
 
 ;; (defn ^:export connect []
@@ -23,11 +23,11 @@
 ;;     (.log js/console "Dunnit"))
 
 (defn clicked [x]
-  (.log js/console (str "You clicked on " x))
+  (dom/log (str "You clicked on " x))
   )
 
 (defn ^:export init []
   (if (and js/document
            (.-getElementById js/document))
     (doseq [i (range 9)]
-      (ev/listen! (by-id (str i)) :click #(clicked (str i))))))
+      (ev/listen! (dom/by-id (str i)) :click #(clicked (str i))))))
