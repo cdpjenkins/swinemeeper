0a1d0d00715dd3b61f7eabbfc19bbe6c37fbff90
Chris Jenkins <cdpjenkins@gmail.com>
2014-06-09 09:36:56 +0100

    Move Clojurescript to :dev dependencies.

--- project.clj
+++ project.clj
@@ -1,17 +1,19 @@
 (defproject swinemeeper "0.10"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2227"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "1.1.8"]
                  [hiccup "1.0.5"]
-                 [hiccups "0.3.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [domina "1.0.2"]
-                 [cljs-ajax "0.2.4"]
                  [fogus/ring-edn "0.2.0"]]
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.10"]
+            [org.clojars.cdpjenkins/lein-ring "0.8.11-SNAPSHOT"]
             [cider/cider-nrepl "0.6.0"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2227"]
+                                  [hiccups "0.3.0"]
+                                  [cljs-ajax "0.2.4"]
+]}}
 
   :source-paths ["src"]
   :hooks [leiningen.cljsbuild]
@@ -23,8 +25,10 @@
                                    :optimizations :whitespace
                                    :pretty-print true}}}}
   :min-lein-version "2.0.0"
-  :ring {:handler swinemeeper.core/app}
+  :ring {:handler swinemeeper.core/app
+         :servlet-version "2.5"
+         :uberwar-name "swinemeeper.war"}
   :main swinemeeper.core
   :uberjar-name "swinemeeper.jar"
-  :uberwar-name "swinemeeper.war")
+)
 
