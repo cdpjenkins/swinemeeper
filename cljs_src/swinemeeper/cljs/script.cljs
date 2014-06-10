220767c12ea8f7ceed3519ab9e2662c2390f885b
Chris Jenkins <cdpjenkins@gmail.com>
2014-06-10 23:32:18 +0100

    Added radio buttons for game type and added event listeners to them.

--- cljs_src/swinemeeper/cljs/script.cljs
+++ cljs_src/swinemeeper/cljs/script.cljs
@@ -1,9 +1,9 @@
 (ns swinemeeper.cljs.script
   (:require [domina :as dom]
+            [domina.xpath :refer [xpath]]
             [domina.events :as ev]
             [hiccups.runtime :as hiccupsrt]
-            [ajax.core :refer [GET POST]]
-            )
+            [ajax.core :refer [GET POST]])
   (:require-macros [hiccups.core :as h]))
 
 (defn log [& rest]
@@ -35,46 +35,59 @@
                          [:img {:src (str "images/:unknown.png")
                                 :id (str x "_" y)}]])])]
                   [:form {:id "new-game-form"}
-                   [:fieldset
+                   [:fieldset {:id "game-type"}
                     [:label
                      "Easy"
-                     [:input {:type "radio" :name "type" :checked "no"}]]
+                     [:input {:type "radio" :name "type" :value "Easy" :id "easy-button"}]]
                     [:label
                      "Medium"
-                     [:input {:type "radio" :name "type" :checked "no"}]]
+                     [:input {:type "radio" :name "type" :value "Medium" :id "medium-button"}]]
                     [:label
                      "Hard"
-                     [:input {:type "radio" :name "type" :checked "no"}]]
+                     [:input {:type "radio" :name "type" :value "Hard" :id "hard-button"}]]
                     [:input {:type "button"
                              :value "New Game"
                              :id "new-game"}]]])))
 
-    (doseq [x (range (:height board))
-            y (range (:width board))]
-      (ev/listen! (dom/by-id (str x "_" y))
-                  :click
-                  (fn [event]
-                    (POST "ajax-click"
-                          {:params {:x x
-                                    :y y}
-                           :handler (fn [response]
-                                      (update-thar-board response))})))
-      (ev/listen! (dom/by-id (str x "_" y))
-                  :contextmenu
-                  (fn [event]
-                    (POST "ajax-right-click"
-                          {:params {:x x
-                                    :y y}
-                           :handler (fn [response]
-                                      (update-thar-board response))})
-                    (ev/prevent-default event))))
-    (ev/listen! (dom/by-id "new-game")
+  (ev/listen! (dom/by-id "easy-button")
+              :click
+              #(log "easy"))
+
+  (ev/listen! (dom/by-id "medium-button")
+              :click
+              #(log "medium"))
+
+  (ev/listen! (dom/by-id "hard-button")
+              :click
+              #(log "hard"))
+
+  (doseq [x (range (:height board))
+          y (range (:width board))]
+    (ev/listen! (dom/by-id (str x "_" y))
                 :click
                 (fn [event]
-                  (POST "ajax-new-board "
-                        {:params {}
+                  (POST "ajax-click"
+                        {:params {:x x
+                                  :y y}
                          :handler (fn [response]
-                                    (create-board response))}))))
+                                    (update-thar-board response))})))
+    (ev/listen! (dom/by-id (str x "_" y))
+                :contextmenu
+                (fn [event]
+                  (POST "ajax-right-click"
+                        {:params {:x x
+                                  :y y}
+                         :handler (fn [response]
+                                    (update-thar-board response))})
+                  (ev/prevent-default event))))
+  
+  (ev/listen! (dom/by-id "new-game")
+              :click
+              (fn [event]
+                (POST "ajax-new-board "
+                      {:params {}
+                       :handler (fn [response]
+                                  (create-board response))}))))
 
 (defn update-thar-board [board]
   (doseq [x (range (:height board))
@@ -89,7 +102,6 @@
   (POST "ajax-new-board "
                       {:params {}
                        :handler (fn [response]
-                                  (create-board response))})
-  )
+                                  (create-board response))}))
 
 (set! (.-onload js/window) init)
