8316832a8347c5189eca825770bea5238dc38227
Chris Jenkins <cdpjenkins@gmail.com>
2014-07-02 20:46:37 +0100

    Screw around with the tests a bit

--- test/swinemeeper/board_test.clj
+++ test/swinemeeper/board_test.clj
@@ -8,52 +8,44 @@
         (assoc :swines swines)
         (assoc :state :game-playing))))
 
+(deftest test-initial-board-2x2
+  (is (= (:type board-2x2) "Custom"))
+  (is (= (board-2x2 [0 0]) :unknown))
+  (is (= (board-2x2 [0 1]) :unknown))
+  (is (= (board-2x2 [1 0]) :unknown))
+  (is (= (board-2x2 [1 1]) :unknown)))
+
 (deftest test-initial-state-pregame
-  (let [board (make-board 10 10 10 "Cheese")]
+  (let [width 7
+        height 8
+        num-swines 10
+        board (make-board num-swines width height "Cheese")]
     (is (= (:state board) :pregame))
-    (is (= (:type board) "Cheese"))))
+    (is (= (:type board) "Cheese"))
+    (is (= (:num-swines board) num-swines))
+    (is (= (:remaining-swines board) num-swines))
+    (is (= (:width board) width))
+    (is (= (:height board) height))
+    (doseq [x (range width)
+            y (range height)]
+      (is (= (:unknown (board [x y])))))))
 
 (deftest test-uncover-bad-2x2
   (is (= (uncover board-2x2 [[0 0]])
-         {[0 0]   :exploding-swine
-          [0 1]   :unknown
-          [1 0]   :unknown
-          [1 1]   :unknown
-          :width  2
-          :height 2
-          :state  :game-lost
-          :num-swines 1
-          :remaining-swines 1
-          :swines (:swines board-2x2)
-          :type "Custom"})))
+         (-> board-2x2
+             (assoc [0 0] :exploding-swine)
+             (assoc :state :game-lost)))))
 
 (deftest test-uncover-good-2x2
   (is (= (uncover board-2x2 [[1 1]])
-           {[0 0]   :unknown
-            [0 1]   :unknown
-            [1 0]   :unknown
-            [1 1]   1
-            :width  2
-            :height 2
-            :state  :game-playing
-            :num-swines 1
-            :remaining-swines 1
-            :swines (:swines board-2x2)
-            :type "Custom"})))
+         (-> board-2x2
+             (assoc [1 1] 1)))))
 
 (deftest test-mark-good-2x2
   (is (= (mark board-2x2 [0 0])
-         {[0 0]   :marked
-          [0 1]   :unknown
-          [1 0]   :unknown
-          [1 1]   :unknown
-          :width  2
-          :height 2
-          :state  :game-playing
-          :num-swines 1
-          :remaining-swines 0
-          :swines (:swines board-2x2)
-          :type "Custom"})))
+         (-> board-2x2
+             (assoc [0 0] :marked)
+             (assoc :remaining-swines 0)))))
 
 (deftest test-win-2x2
   (is (= (uncover board-2x2 [[1 0] [0 1] [1 1]])
