71858ab81f8f94c066bed821a7824add2468a40e
Chris Jenkins <chris.jenkins@masabi.com>
2018-04-04 13:23:15 +0100

    Re-jig integration test a little

--- test/swinemeeper/integration_test.clj
+++ test/swinemeeper/integration_test.clj
@@ -4,38 +4,40 @@
   (:require [etaoin.keys :as k]
             [swinemeeper.routes :as sm]))
 
-
 (def ^:dynamic
   *driver*)
 (def ^:dynamic
   *server*)
 
-(defn fixture-driver
+(defn with-server-fixture [f]
+  (let [server (sm/make-server)]
+    (binding [*server* server]
+      (f)
+      (.stop *server*))))
+
+(defn webdriver-fixture
   "Executes a test running a driver. Bounds a driver
    with the global *driver* variable."
   [f]
-  (let [server (sm/make-server)]
-    (with-phantom {} driver
-      (binding [*driver* driver]
-        (f)))
-    (.stop server)))
+  (with-phantom {} driver
+    (binding [*driver* driver]
+      (f))))
 
-(use-fixtures
-  :each ;; start and stop driver for each test
-  fixture-driver)
+(use-fixtures :each webdriver-fixture with-server-fixture)
 
 (deftest smoke-test
   "Ensure that the game can at least start"
   (go *driver* "http://localhost:8000")
-  (wait 3)
+  (wait 1)
   (is (=
        (get-element-text *driver* {:id "game-state"})
        "Pregame"))
   (click *driver* {:id "5_5"})
-  (wait 1)
+  (wait 2)
   (is (=
        (get-element-text *driver* {:id "game-state"})
        "Game Playing"))
+  (wait 1)
   (is (=
        (get-element-text *driver* {:id "swines-remaining"})
        "10")))
